ef3f627033bf9208a833354fc27e0d3818a2aa86
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-16 17:20:06 -0600

    Add read/write functions

--- HillCipher/Src/Main.cpp
+++ HillCipher/Src/Main.cpp
@@ -1,13 +1,45 @@
 // Copyright (c) 2021 Tobias Briones. All rights reserved.
 
 #include <iostream>
+#include <fstream>
 #include "HillCipher.h"
 #include "SquareMatrix.h"
 #include "Test.h"
 
-using namespace std;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+
+const string DEC_FILE_NAME = "dec.txt";
+const string ENC_FILE_NAME = "enc.txt";
+
+string read(string fileName);
+
+void write(string str, string fileName);
 
 int main()
 {
+    string str = read(ENC_FILE_NAME);
+
+    cout << str << endl;
+
+    write("messg", DEC_FILE_NAME);
     testAll();
+}
+
+string read(string fileName)
+{
+    string str;
+    ifstream file(fileName);
+    
+    while (getline(file, str));
+    return str;
+}
+
+void write(string str, string fileName)
+{
+    ofstream file(fileName);
+
+    file << str;
+    file.close();
 }

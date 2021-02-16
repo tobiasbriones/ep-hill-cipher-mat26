c72d5ffd2732357192911afb70f8be91b5063262
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-16 17:54:55 -0600

    Create SquareMatrix26

---
+++ HillCipher/Src/SquareMatrix26.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Tobias Briones. All rights reserved.
+
+#pragma once
+
+#include "SquareMatrix.h"
+
+class SquareMatrix26 : SquareMatrix
+{
+public:
+	SquareMatrix26* operator+(const SquareMatrix26&) const;
+};

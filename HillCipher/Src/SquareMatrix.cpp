dbabb74666df83d4e7f715e252bfa260bba4e11d
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-15 21:14:35 -0600

    Implement SquareMatrix

--- HillCipher/Src/SquareMatrix.cpp
+++ HillCipher/Src/SquareMatrix.cpp
@@ -1,3 +1,108 @@
 // Copyright (c) 2021 Tobias Briones. All rights reserved.
 
 #include "SquareMatrix.h"
+
+SquareMatrix::SquareMatrix(int n) :
+	n(n)
+{
+	matrix = new int* [n];
+
+	for (int i = 0; i < n; i++)
+	{
+		matrix[i] = new int[n];
+
+		for (int j = 0; j < n; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+}
+
+SquareMatrix::~SquareMatrix()
+{
+	for (int i = 0; i < n; i++)
+	{
+		delete [] matrix[i];
+	}
+	delete [] matrix;
+}
+
+int* SquareMatrix::operator[](int i) const {
+	return matrix[i];
+}
+
+SquareMatrix* SquareMatrix::operator+(const SquareMatrix& other) const 
+{
+	if (n != other.size()) {
+		throw "Matrices must have the same size";
+	}
+	auto *result = new SquareMatrix(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			result->set(i, j, matrix[i][j] + other[i][j]);
+		}
+	}
+	return result;
+}
+
+int SquareMatrix::size() const
+{
+	return n;
+}
+
+void SquareMatrix::set(int i, int j, int value) {
+	matrix[i][j] = value;
+}
+
+int SquareMatrix::det()
+{
+	return computeDeterminant(*this);
+}
+
+SquareMatrix* SquareMatrix::inv() const
+{
+	auto* result = new SquareMatrix(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+		}
+	}
+	return result;
+}
+
+int SquareMatrix::computeDeterminant(SquareMatrix& mat)
+{
+	int n = mat.size();
+
+	if (n == 2)
+	{
+		return (mat[0][0] * mat[1][1]) - (mat[1][0] * mat[0][1]);
+	}
+	int det = 0;
+	SquareMatrix submat(n - 1);
+
+	for (int x = 0; x < n; x++) {
+		int subi = 0;
+
+		for (int i = 1; i < n; i++) {
+			int subj = 0;
+
+			for (int j = 0; j < n; j++) {
+				if (j == x)
+				{
+					continue;
+				}
+				submat[subi][subj] = mat[i][j];
+				subj++;
+			}
+			subi++;
+		}
+		det = det + (pow(-1, x) * mat[0][x] * computeDeterminant(submat));
+	}
+	return det;
+}

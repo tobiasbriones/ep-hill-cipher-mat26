b14fb2169986e41d3d68432ecdfe2223d2151077
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-15 19:36:09 -0600

    Define SquareMatrix

--- HillCipher/Src/SquareMatrix.h
+++ HillCipher/Src/SquareMatrix.h
@@ -1,3 +1,27 @@
 // Copyright (c) 2021 Tobias Briones. All rights reserved.
 
 #pragma once
+
+#include<math.h>
+
+class SquareMatrix
+{
+
+private:
+	int n;
+	int** matrix;
+
+	static int computeDeterminant(SquareMatrix&);
+
+public:
+	SquareMatrix(int);
+	~SquareMatrix();
+
+	int* operator[](int) const;
+	SquareMatrix* operator+(const SquareMatrix&) const;
+	int size() const;
+	void set(int, int, int);
+	int det();
+	SquareMatrix* inv() const;
+
+};

31a02afc042a73e4e5095423e1f52a16a7622961
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-16 22:49:46 -0600

    Format

--- HillCipher/Src/SquareMatrix26.cpp
+++ HillCipher/Src/SquareMatrix26.cpp
@@ -42,8 +42,10 @@ SquareMatrix* SquareMatrix26::inv() const
 	// TODO implement scalar multiplication operator
 	auto factor = modInverse(determinant, 26);
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+	for (int i = 0; i < n; i++) 
+	{
+		for (int j = 0; j < n; j++) 
+		{
 			auto product = factor * (*adjPtr)[i][j];
 			(*invPtr)[i][j] = mod26(product);
 		}
@@ -57,7 +59,8 @@ vector<int>* SquareMatrix26::toVec26(vector<int>& vec)
 	auto n = vec.size();
 	auto vec26Ptr = new vector<int>(n);
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
+	{
 		(*vec26Ptr)[i] = mod26(vec[i]);
 	}
 	return vec26Ptr;
@@ -68,8 +71,10 @@ SquareMatrix26* SquareMatrix26::toMat26(SquareMatrix& mat)
 	auto n = mat.size();
 	auto mat26Ptr = new SquareMatrix26(n);
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
+	for (int i = 0; i < n; i++) 
+	{
+		for (int j = 0; j < n; j++) 
+		{
 			(*mat26Ptr)[i][j] = mod26(mat[i][j]);
 		}
 	}

a669ae0de80a28e24ab68a5d4337f365f03ac674
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-16 19:05:57 -0600

    Create Alphabet

---
+++ HillCipher/Src/Alphabet.h
@@ -0,0 +1,22 @@
+#pragma once
+
+struct Alphabet26 {
+	char chars[26] = {
+			'A', 'B', 'C', 'D', 'E', 'F', 'G',
+			'H', 'I', 'J', 'K', 'L', 'M', 'N',
+			'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+			'V', 'W', 'X', 'Y', 'Z'
+	};
+
+	Alphabet26() {}
+
+	int length()
+	{
+		return 26;
+	}
+
+	int canonicalPositionOf(char ch)
+	{
+		return (int)ch - 65;
+	}
+};

a15e5bfb1446a84c0bc7a1ccea1d34045f453d7b
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-02-16 18:17:03 -0600

    Implement Mat26 operator overloading

--- HillCipher/Src/SquareMatrix26.cpp
+++ HillCipher/Src/SquareMatrix26.cpp
@@ -1,3 +1,48 @@
 // Copyright (c) 2021 Tobias Briones. All rights reserved.
 
 #include "SquareMatrix26.h"
+
+
+SquareMatrix26::SquareMatrix26(int n) : SquareMatrix(n) {}
+
+SquareMatrix26* SquareMatrix26::operator+(const SquareMatrix26& other) const
+{
+	auto baseSumPtr = sum(other);
+	auto sumPtr = toMat26(*baseSumPtr);
+
+	delete baseSumPtr;
+	return sumPtr;
+}
+
+vector<int>* SquareMatrix26::operator*(const vector<int>& vec) const
+{
+	auto baseMulPtr = mul(vec);
+	auto mulPtr = toVec26(*baseMulPtr);
+
+	delete baseMulPtr;
+	return mulPtr;
+}
+
+vector<int>* SquareMatrix26::toVec26(vector<int>& vec)
+{
+	auto n = vec.size();
+	auto vec26Ptr = new vector<int>(n);
+
+	for (int i = 0; i < n; i++) {
+		(*vec26Ptr)[i] = mod26(vec[i]);
+	}
+	return vec26Ptr;
+}
+
+SquareMatrix26* SquareMatrix26::toMat26(SquareMatrix& mat)
+{
+	auto n = mat.size();
+	auto mat26Ptr = new SquareMatrix26(n);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			(*mat26Ptr)[i][j] = mod26(mat[i][j]);
+		}
+	}
+	return mat26Ptr;
+}
--- HillCipher/Src/SquareMatrix26.h
+++ HillCipher/Src/SquareMatrix26.h
@@ -4,8 +4,17 @@
 
 #include "SquareMatrix.h"
 
-class SquareMatrix26 : SquareMatrix
+class SquareMatrix26 : public SquareMatrix
 {
+
+private:
+	static vector<int>* toVec26(vector<int>&);
+	static SquareMatrix26* toMat26(SquareMatrix&);
+
 public:
+	SquareMatrix26(int);
+
 	SquareMatrix26* operator+(const SquareMatrix26&) const;
+	vector<int>* operator*(const vector<int>&) const;
+
 };

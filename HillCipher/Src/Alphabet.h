eebd37124d5f37f842ff39b41179001811dd1f86
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-03-29 19:05:30 -0600

    Add licence notice header

--- HillCipher/Src/Alphabet.h
+++ HillCipher/Src/Alphabet.h
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Example Project: Hill Cipher Mat26.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 #pragma once
 
 struct Alphabet26
--- HillCipher/Src/Test.h
+++ HillCipher/Src/Test.h
@@ -1,4 +1,14 @@
-// Copyright (c) 2021 Tobias Briones. All rights reserved.
+/*
+ * Copyright (c) 2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of Example Project: Hill Cipher Mat26.
+ *
+ * This source code is licensed under the MIT License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/MIT.
+ */
 
 #pragma once
 
